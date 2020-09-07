fa27d86bad7aa43582ba351ae383ddaf7a7a1f25
Thibault bui Koechlin <thibault@crowdsec.net>
2020-09-07 15:20:49 +0200

    up

--- pkg/apiclient/alerts.go
+++ pkg/apiclient/alerts.go
@@ -1,9 +1,15 @@
 package apiclient
 
+import (
+	"context"
+
+	"github.com/crowdsecurity/crowdsec/pkg/models"
+)
+
 // type ApiAlerts service
 
 type AlertsService service
 
-func (c *AlertsService) Add() {
-
+func (c *AlertsService) Add(ctx context.Context) (*models.AddAlertsResponse, *Response, error) {
+	return nil, nil, nil
 }
