be6da02bc5f88c6a3b823e2a8a653b2a1321a27a
erenJag <erenJag>
2020-09-08 18:29:57 +0200

    add jwt authentication

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -9,9 +9,9 @@ import (
 	"os"
 	"time"
 
-	"github.com/crowdsecurity/crowdsec/pkg/apiserver/middlewares"
-
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/crowdsecurity/crowdsec/pkg/apiserver/controllers"
+	"github.com/crowdsecurity/crowdsec/pkg/apiserver/middlewares"
 	"github.com/crowdsecurity/crowdsec/pkg/csconfig"
 	"github.com/crowdsecurity/crowdsec/pkg/database"
 	"github.com/crowdsecurity/crowdsec/pkg/database/ent"
@@ -21,7 +21,8 @@ import (
 )
 
 var (
-	keyLength = 32
+	keyLength        = 32
+	apiKeyHeaderName = "X-Api-Key"
 )
 
 type APIServer struct {
@@ -49,6 +50,10 @@ func NewServer(config *csconfig.CrowdSec) (*APIServer, error) {
 }
 
 func (s *APIServer) Run() {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = "crowdsecret"
+	}
 	controller := controllers.New(s.ctx, s.dbClient, middlewares.APIKeyHeader)
 
 	defer controller.Client.Close()
@@ -75,20 +80,62 @@ func (s *APIServer) Run() {
 
 	router.Use(gin.Recovery())
 
+	// init jwt middleware
+	jwtMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:           "Crowdsec API local",
+		Key:             []byte(secret),
+		Timeout:         time.Hour * 24,
+		MaxRefresh:      time.Hour * 24,
+		IdentityKey:     "id",
+		PayloadFunc:     middlewares.PayloadFunc,
+		IdentityHandler: middlewares.IdentityHandler,
+		Authenticator:   middlewares.Authenticator,
+		Authorizator:    middlewares.Authorizator,
+		Unauthorized:    middlewares.Unauthorized,
+		TokenLookup:     "header: Authorization, query: token, cookie: jwt",
+		TokenHeadName:   "Bearer",
+		TimeFunc:        time.Now,
+	})
+
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
+	errInit := jwtMiddleware.MiddlewareInit()
+	if errInit != nil {
+		log.Fatal("authMiddleware.MiddlewareInit() Error:" + errInit.Error())
+	}
+
+	router.POST("/watchers/login", jwtMiddleware.LoginHandler)
+
+	//router.NoRoute(jwtMiddleware.MiddlewareFunc(), func(c *gin.Context) {
+	//	_ = jwt.ExtractClaims(c)
+	//	c.JSON(http.StatusNotFound, gin.H{"error": "Page not found"})
+	//})
+
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+		c.JSON(http.StatusOK, gin.H{"message": "hello world"})
 	})
 	router.POST("/machines", controller.CreateMachine)
-	router.POST("/alerts", controller.CreateAlert)
-	router.GET("/alerts", controller.FindAlerts)
-	router.DELETE("/alerts", controller.DeleteAlerts)
 
-	apiKeyAuth := router.Group("/")
-	apiKeyAuth.Use(middlewares.APIKeyRequired(controller))
+	jwtAuth := router.Group("/")
+	jwtAuth.GET("/refresh_token", jwtMiddleware.RefreshHandler)
+	jwtAuth.Use(jwtMiddleware.MiddlewareFunc())
 	{
-		apiKeyAuth.GET("/decisions", controller.GetDecision)
-		apiKeyAuth.GET("/decisions/stream", controller.StreamDecision)
+		// /decisions
+		jwtAuth.GET("/decisions", controller.GetDecision)
+
+		// /alerts
+		jwtAuth.POST("/alerts", controller.CreateAlert)
+		jwtAuth.GET("/alerts", controller.FindAlerts)
+		jwtAuth.DELETE("/alerts", controller.DeleteAlerts)
 	}
+	//apiKeyAuth := router.Group("/")
+	//apiKeyAuth.Use(middlewares.APIKeyRequired(controller))
+	//{
+	//	apiKeyAuth.GET("/decisions", controller.GetDecision)
+	//	apiKeyAuth.GET("/decisions/stream", controller.StreamDecision)
+	//}
 
 	router.Run(s.url)
 }
